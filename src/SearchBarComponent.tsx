593e42b1fb9d13c587a2d7cb6000cd570df00861
dldydtn0805 <139421083+dldydtn0805@users.noreply.github.com>
2025-05-27 05:27:07 +0900

    [Refactoring] app.css, countryCode.ts, SearchBarComponent.tsx

--- src/SearchBarComponent.tsx
+++ src/SearchBarComponent.tsx
@@ -6,26 +6,34 @@ const SearchBarComponent = () => {
   const queryClient = useQueryClient();
   const apiKey = import.meta.env.VITE_YOUTUBE_API_KEY;
 
-  const clickCountyCodeButton = async (countryCode : string) => {
-      
-      const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&regionCode=${countryCode}&maxResults=10&key=${apiKey}`
-      await queryClient.fetchQuery<YouTubeVideoListResponse>({
-        queryKey: ["search"],
-        queryFn: async () => {
-          const response = await fetch(url);
-          const result: YouTubeVideoListResponse = await response.json();
-          console.log("Search Result : ", result);
-          return result;
-        },
-        staleTime: 1000 * 10
-      })
-  }
-  
-  return (<div>
-    {countryCodes.map((countryCode)=> {
-      return <button onClick={()=>clickCountyCodeButton(countryCode)}> {countryCode}</button>
-    })}
-  </div>)
-}
+  const clickCountyCodeButton = async (countryCode: string) => {
+    const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&regionCode=${countryCode}&maxResults=10&key=${apiKey}`;
+    await queryClient.fetchQuery<YouTubeVideoListResponse>({
+      queryKey: ["search"],
+      queryFn: async () => {
+        const response = await fetch(url);
+        const result: YouTubeVideoListResponse = await response.json();
+        console.log("Search Result : ", result);
+        return result;
+      },
+      staleTime: 1000 * 10
+    });
+  };
+
+  return (
+      <div>
+        {countryCodes.map((country) => {
+          return (
+              <button
+                  key={country.code}
+                  onClick={() => clickCountyCodeButton(country.code)}
+              >
+                {country.name}
+              </button>
+          );
+        })}
+      </div>
+  );
+};
 
 export default SearchBarComponent;
